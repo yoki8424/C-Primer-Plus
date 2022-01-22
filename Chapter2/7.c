6b5ad2d99e4161b73d16b94d92b5ad66daf6669b
yoki8424 <loveyoyowon@gmail.com>
2022-01-23 01:10:36 +0800

    7

---
+++ Chapter2/7.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+//函数原型
+void smile(void);
+//定义函数
+void smile(void)
+{
+	printf("Smile!");
+}
+
+int main()
+{
+	smile();
+	smile();
+	smile();
+	printf("\n");
+	smile();
+	smile();
+	printf("\n");
+	smile();
+	printf("\n");
+
+	return 0;
+}
