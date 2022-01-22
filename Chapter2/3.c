3565f85b22f37f37ba292051a60b313279f36bd2
yoki8424 <loveyoyowon@gmail.com>
2022-01-22 22:46:43 +0800

    3

---
+++ Chapter2/3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	int my_age = 25;
+	int days_per_year = 365;
+	printf("I am %d years old.\n", my_age);
+	printf("Days are %d.\n", my_age * days_per_year);
+
+	return 0;
+}
