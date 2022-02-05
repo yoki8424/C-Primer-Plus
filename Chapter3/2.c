e7861521112bf5fcbe54a25a8b7d847d81600a8e
yoki8424 <loveyoyowon@gmail.com>
2022-02-06 03:25:36 +0800

    2

---
+++ Chapter3/2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+	int input = 0;
+	printf("Enter a value of char int ASCII: \n");
+	scanf("%d", &input);
+	printf("Your input value is %d, and char is %c\n", input, input);
+	return 0;
+}
